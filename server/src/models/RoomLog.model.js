09fda78b8e0163c0be53df9241323f9907c941e8
Deeapk Singh <deepakksingh1202@gmail.com>
2026-07-05 17:13:13 +0530

    add controllers

--- server/src/models/RoomLog.model.js
+++ server/src/models/RoomLog.model.js
@@ -9,31 +9,33 @@ const roomLogSchema = new mongoose.Schema(
       required: true, 
       unique: true, 
       index: true 
-
     },
+
     isPrivate: { 
       type: Boolean,
       default: false 
-
     },
+
     settings: { 
       type: mongoose.Schema.Types.Mixed 
-
     },
+
     state: {
       type: String,
       enum: Object.values(ROOM_STATE),
       default: ROOM_STATE.WAITING,
     },
+
     peakPlayerCount: { 
       type: Number, 
       default: 0 
-
     },
+
     startedAt: Date,
     endedAt: Date,
-  },
-  { timestamps: true }
+  },{ 
+    timestamps: true 
+  }
 );
 
 export default mongoose.model('RoomLog', roomLogSchema);
